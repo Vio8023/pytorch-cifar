3241a03a0fb6c35ec34fd74aee547c4b2e13d885
Allen Shi <haoranshi97@gmail.com>
2019-05-03 17:06:33 -0400

    add s&p noise

--- noise.py
+++ noise.py
@@ -5,43 +5,40 @@ def noise_data(noise_type):
     if noise_type == "gauss":
         def _noise(image):
             image = image.numpy()
-            print(image.shape)
-            mean, sigma = 0.5, 0.1
+            mean, sigma = 0, 0.1
             gauss = np.random.normal(mean, sigma, image.shape)
             noisy = image + gauss
             return torch.FloatTensor(noisy)
         
-    if noise_type == "s&p":
+    elif noise_type == "s&p":
         def _noise(image):
-            row,col,ch = image.shape
+            image = image.numpy()
             s_vs_p = 0.5
             amount = 0.004
-            out = np.array(image).copy()
+            out = image.copy()
             # Salt mode
             num_salt = np.ceil(amount * image.size * s_vs_p)
             coords = [np.random.randint(0, i - 1, int(num_salt))
                       for i in image.shape]
-            out[coords] = 1
+            out[tuple(coords)] = 1
             # Pepper mode
             num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
             coords = [np.random.randint(0, i - 1, int(num_pepper))
                       for i in image.shape]
-            out[coords] = 0
-            return out
-    if noise_type == "poisson":
+            out[tuple(coords)] = -1
+            return torch.FloatTensor(out)
+        
+    elif noise_type == "speckle":
         def _noise(image):
-            vals = len(np.unique(image))
-            vals = 2 ** np.ceil(np.log2(vals))
-            noisy = np.random.poisson(image * vals) / float(vals)
-            return noisy
-    if noise_type == "speckle":
-        def _noise(image):
+            image = image.numpy()
             row,col,ch = image.shape
             gauss = np.random.randn(row,col,ch)
             gauss = gauss.reshape(row,col,ch)        
             noisy = image + image * gauss
-            return noisy
-
+            return torch.FloatTensor(noisy)
+    else:
+        raise ValueError("the noise type {} is not defined".format(noise_type))
+        
     return _noise
 
 def recover_image(input_array):
